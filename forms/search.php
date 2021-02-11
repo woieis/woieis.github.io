5e680a6455531ab5d19e91880cf40fa2461552aa
woieis <iskandar.ict@gmail.com>
2021-02-11 20:36:13 +0800

    XSS

---
+++ forms/search.php
@@ -0,0 +1,38 @@
+<?php
+mysql_connect("localhost","root","") or die("could not connect");
+mysql_select_db("kkp") or die("could not find db!");
+$output ='';
+print_r($_POST);
+// ...
+//collect
+if (isset($_POST['search'])){
+    $searchq = $_POST['search'];
+    $searchq = preg_replace("#[^0-9a-z]#i","",$searchq);
+    $query = mysql_query("SELECT * FROM personal_info WHERE FirstName LIKE '%$searchq%' OR SurName LIKE '%$searchq%'") or die("could not search");
+    $count = mysql_num_rows($query);
+    if($count == 0){
+        $output = 'There was no search results!';
+    }else{
+        while($row = mysql_fetch_array($query)){
+            $fname = $row['FirstName'];
+            $lname = $row['SurName'];
+            $id = $row['id'];
+
+            $output .= '<div>'.$fname.''.$lname.'</div>';
+        }
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Search</title>
+</head>
+<body>
+    <form method="post" action="/forms/search.php"></form>
+        <input type="text" name="search" placeholder="Search for student">
+        <input type="submit" value="Submit">
+</body>
+</html>
+
+<?php print("$output");?>
